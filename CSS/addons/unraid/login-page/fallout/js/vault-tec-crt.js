bfdd50dba01754d34de5246f86ccfad09273097e
Marius <24592972+gilbN@users.noreply.github.com>
2021-03-15 19:56:46 +0100

    video background fix

--- CSS/addons/unraid/login-page/fallout/js/vault-tec-crt.js
+++ CSS/addons/unraid/login-page/fallout/js/vault-tec-crt.js
@@ -4,4 +4,4 @@
 let video = `
 <div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/gilbn.github.io\/theme.park\/CSS\/addons\/unraid\/login-page\/fallout\/video\/VaultTecCRTLoop.mp4" type="video\/mp4">Video not supported</video></div>
 `
-document.body.innerHTML += video
+document.body.innerHTML += video;
