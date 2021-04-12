416777fb1555a8c4ba763182625c2bf54207036a
marcosnajera <82390291+marcosnajera@users.noreply.github.com>
2021-04-12 08:34:19 -0700

    Update custom_login.sh

--- CSS/addons/unraid/login-page/custom_login.sh
+++ CSS/addons/unraid/login-page/custom_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TYPE="retro-terminal"
 THEME="green.css"
-DOMAIN="gilbn.github.io" #if you update the domain after the script has been run, You must disable and re enable JS or the whole theme.  
+DOMAIN="github.com/marcosnajera" #if you update the domain after the script has been run, You must disable and re enable JS or the whole theme.  
 ADD_JS="true"
 JS="custom_text_header.js"
 DISABLE_THEME="false"
