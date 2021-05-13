b58aa26ef3cb7533572bdcd96b2c22b937feb6d7
Marius <24592972+gilbN@users.noreply.github.com>
2021-05-13 18:56:41 +0200

    domain fixes for unraid login theme

--- CSS/addons/unraid/login-page/custom_login.sh
+++ CSS/addons/unraid/login-page/custom_login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TYPE="retro-terminal"
 THEME="green.css"
-DOMAIN="gilbn.github.io" #if you update the domain after the script has been run, You must disable and re enable JS or the whole theme.  
+DOMAIN="theme-park.dev" #if you update the domain after the script has been run, You must disable and re enable JS or the whole theme.  
 ADD_JS="true"
 JS="custom_text_header.js"
 DISABLE_THEME="false"
@@ -26,34 +26,42 @@ if [ ! -f /usr/local/emhttp/login.php.backup ]; then
   cp -p /usr/local/emhttp/login.php /usr/local/emhttp/login.php.backup
 fi
 
+# Use correct domain style
+case ${DOMAIN} in
+  *"github.io"*)
+  echo "Switching to github.io URL style"
+    DOMAIN="${DOMAIN}\/theme.park"
+    ;;
+esac
+
 # Adding stylesheets
 if ! grep -q ${DOMAIN} /usr/local/emhttp/login.php; then
   echo "Adding stylesheet"
-  sed -i -e "\@<style>@i\    <link rel='stylesheet' href='https://${DOMAIN}/theme.park/CSS/addons/unraid/login-page/${TYPE}/${THEME}'>" /usr/local/emhttp/login.php
+  sed -i -e "\@<style>@i\    <link rel='stylesheet' href='https://${DOMAIN}/CSS/addons/unraid/login-page/${TYPE}/${THEME}'>" /usr/local/emhttp/login.php
   echo 'Stylesheet set to' ${THEME}
 fi
 
 # Adding/Removing javascript
 if [ ${ADD_JS} = "true" ]; then
   if ! grep -q ${JS} /usr/local/emhttp/login.php; then
-    if grep -q "<script type='text/javascript' src='https://${DOMAIN}/theme.park/CSS/addons/unraid/login-page/" /usr/local/emhttp/login.php; then
+    if grep -q "<script type='text/javascript' src='https://${DOMAIN}/CSS/addons/unraid/login-page/" /usr/local/emhttp/login.php; then
       echo "Replacing Javascript"
-      sed -i "/<script type='text\/javascript' src='https:\/\/${DOMAIN}\/theme.park\/CSS\/addons\/unraid\/login-page/c <script type='text/javascript' src='https://${DOMAIN}/theme.park/CSS/addons/unraid/login-page/${TYPE}/js/${JS}'></script>" /usr/local/emhttp/login.php
+      sed -i "/<script type='text\/javascript' src='https:\/\/${DOMAIN}\/CSS\/addons\/unraid\/login-page/c <script type='text/javascript' src='https://${DOMAIN}/CSS/addons/unraid/login-page/${TYPE}/js/${JS}'></script>" /usr/local/emhttp/login.php
     else
       echo "Adding javascript"
-      sed -i -e "\@</body>@i\    <script type='text/javascript' src='https://${DOMAIN}/theme.park/CSS/addons/unraid/login-page/${TYPE}/js/${JS}'></script>" /usr/local/emhttp/login.php
+      sed -i -e "\@</body>@i\    <script type='text/javascript' src='https://${DOMAIN}/CSS/addons/unraid/login-page/${TYPE}/js/${JS}'></script>" /usr/local/emhttp/login.php
     fi
   fi
 else
   if grep -q ${JS} /usr/local/emhttp/login.php; then
     echo "Removing javascript.."
-    sed -i "/<script type='text\/javascript' src='https:\/\/${DOMAIN}\/theme.park\/CSS\/addons\/unraid\/login-page/d" /usr/local/emhttp/login.php
+    sed -i "/<script type='text\/javascript' src='https:\/\/${DOMAIN}\/CSS\/addons\/unraid\/login-page/d" /usr/local/emhttp/login.php
   fi
 fi
 
 # Changing stylesheet
 if ! grep -q ${TYPE}"/"${THEME} /usr/local/emhttp/login.php; then
   echo "Changing existing custom stylesheet.." 
-  sed -i "/<link rel='stylesheet' href='https:\/\/${DOMAIN}\/theme.park\/CSS\/addons\/unraid\/login-page/c <link rel='stylesheet' href='https://${DOMAIN}/theme.park/CSS/addons/unraid/login-page/${TYPE}/${THEME}'>" /usr/local/emhttp/login.php
+  sed -i "/<link rel='stylesheet' href='https:\/\/${DOMAIN}\/CSS\/addons\/unraid\/login-page/c <link rel='stylesheet' href='https://${DOMAIN}/CSS/addons/unraid/login-page/${TYPE}/${THEME}'>" /usr/local/emhttp/login.php
   echo 'Stylesheet set to' ${THEME}
 fi
