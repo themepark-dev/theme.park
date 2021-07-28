fb626a151d25e299d707a6a62e8ac4c9cefd76e6
GilbN <GilbN@users.noreply.github.com>
2021-07-28 18:04:37 +0000

    deploy: 10f5ad90ee4310b47728db895b1104ffab183c96

--- CSS/addons/unraid/login-page/fallout/js/vault-tec-crt_no-scanline.js
+++ CSS/addons/unraid/login-page/fallout/js/vault-tec-crt_no-scanline.js
@@ -1,3 +1,7 @@
-let video=`
+// Adds a video background
+// Source: https://www.youtube.com/watch?v=ehvduomE0AU
+
+let video = `
 <div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/gilbn.github.io\/theme.park\/CSS\/addons\/unraid\/login-page\/fallout\/video\/VaultTecCRTLoopNoScanline.mp4" type="video\/mp4">Video not supported</video></div>
-`;document.body.innerHTML+="\n<div class=\"background-wrap\"><video id=\"video-bg-elem\" preload=\"auto\" autoplay=\"true\" loop=\"loop\" muted=\"muted\"> <source src=\"https://gilbn.github.io/theme.park/CSS/addons/unraid/login-page/fallout/video/VaultTecCRTLoopNoScanline.mp4\" type=\"video/mp4\">Video not supported</video></div>\n";
+`
+document.body.innerHTML += video
--- CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -1,8 +1,16 @@
-let custom_text_header=`
+
+ //Custom Text Header //
+ // ACSII slant font
+
+// Needs to be inside <section id="login" class="shadow">
+// You must escape any backward slashes. "\\"
+
+let custom_text_header = `
 <div class="custom-text-header"> <pre>                     __                             ___       __  
    ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
   / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
  / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
 /_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
                                                                  </pre> </div>
-`;document.getElementById("login").innerHTML+="\n<div class=\"custom-text-header\"> <pre>                     __                             ___       __  \n   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__\n  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/\n / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   \n/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  \n                                                                 </pre> </div>\n";
+`;
+document.getElementById("login").innerHTML += custom_text_header
