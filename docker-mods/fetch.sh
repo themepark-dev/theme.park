a4f2b2da549ebceb7d60a29f18cd9381891a77e2
GilbN <gilberg@gmail.com>
2022-07-10 18:39:49 +0200

    Add docker mods fetch script

---
+++ docker-mods/fetch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+MODS=$(curl https://develop.theme-park.dev/themes.json | jq -r '.["docker-mods"]')
+if [[ "$0" == "bash" ]]; then
+    DIR="/tmp/theme-park-mods"
+else
+  DIR="$0"
+fi
+mkdir -p "$DIR"
+printf "\nSaving mods into $DIR\n\n"
+jq -r 'to_entries | map(.key + "|" + (.value | tostring)) | .[]' <<< "$MODS" | \
+  while IFS='|' read key value; do
+    curl "$value" --create-dirs --output "$DIR/$key" --silent
+    echo "Fetched $key script"
+  done
+sudo chmod go+rx $DIR
