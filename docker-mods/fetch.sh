d4e8fc32d92b5434f47db140811a951f9f6ccc44
GilbN <gilberg@gmail.com>
2022-07-10 18:56:58 +0200

    domain fix

--- docker-mods/fetch.sh
+++ docker-mods/fetch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MODS=$(curl https://develop.theme-park.dev/themes.json | jq -r '.["docker-mods"]')
+MODS=$(curl https://theme-park.dev/themes.json | jq -r '.["docker-mods"]')
 if [[ "$0" == "bash" ]]; then
     DIR="/tmp/theme-park-mods"
 else
